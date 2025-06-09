035fedf7295e31f45aa8682d5cdf490831601e90
heihal <55449594+heihal@users.noreply.github.com>
2025-06-09 11:53:31 +0300

    Update handler.py

--- traffic-generator-func/handler.py
+++ traffic-generator-func/handler.py
@@ -1,8 +1,11 @@
+import os
+
 def generate_traffic(event, context):
     import requests
     import random
 
-    main_app_url = "http://service1-main-app"  # Replace with the actual service URL
+    # Prioritize TARGET_URL from env, fallback to internal service name
+    main_app_url = os.environ.get("TARGET_URL", "http://service1-main-app.hemhal-dev.svc.cluster.local:5000") 
     num_requests = random.randint(1, 10)  # Random number of requests to simulate traffic
 
     for _ in range(num_requests):
