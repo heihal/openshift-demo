1b8e8ff3be024bb050d727c7abaf2ad7ceae2556
heihal <55449594+heihal@users.noreply.github.com>
2025-06-09 14:16:49 +0300

    Update app.py

--- service2-message-app/app.py
+++ service2-message-app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def get_message():
     return jsonify(message="Hello from demo-service 2!")
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify(status="UP"), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
